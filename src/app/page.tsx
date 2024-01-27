d6cb70cf0362ef9fce7b5436b5342c76c22f94ac
edumetz16 <eduardo@linead.com.ar>
2024-01-27 13:35:26 -0300

    remove paddings in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-6 md:p-24">
       <div className="z-10 max-w-5xl w-full items-start justify-start font-mono text-sm lg:flex flex-col gap-10">
         <div>
           <p>Director/writer. Buenos Aires., Argentina 1988. Graduates in Filmmaking from Universidad del Cine (FUC) in 2011. He directed “About Twelve”, recipient of the Fondo Metropolitano de la Ciudad de Buenos Aires grant, which premiered in the National Competition at BAFICI (2014).</p>
